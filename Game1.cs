4e6b0d54b5edef999f091b6810d2c92b4f70f276
Isaac Blanco <nuclearhands2001@gmail.com>
2024-08-07 02:01:51 -0500

    add: is alive and can move (afte reading XNA i hate again Microsoft)

--- Game1.cs
+++ Game1.cs
@@ -1,11 +1,15 @@
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace castelvaniaripoff
 {
     class Game1 : Game
     {
         GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+        Texture2D playerTexture;
 
         public Game1()
         {
@@ -15,6 +19,11 @@ namespace castelvaniaripoff
 
         override protected void Initialize()
         {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO Possibly I need a better way to load the textures for the player, for now I will just load it here.
+            playerTexture = Content.Load<Texture2D>("Graphics/Characters/main");
+            // TODO I need to create a better way to load the player, for now I will just create it here, possible a PlayerManager?.
+            player = new Player("Player", 100, 100, 50, 50, 50, 50, playerTexture);
             base.Initialize();
         }
 
@@ -25,12 +34,19 @@ namespace castelvaniaripoff
 
         override protected void Update(GameTime gameTime)
         {
+            player.Update(gameTime);
             base.Update(gameTime);
         }
 
         override protected void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
+
+            // We need to share the same instance of spriteBatch between the entities. We can say that this is Dependency Injection.
+            // And Singleton Pattern too, Taking this as a reference https://stackoverflow.com/questions/13970726/best-practices-efficient-sprite-drawing-in-xna
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
